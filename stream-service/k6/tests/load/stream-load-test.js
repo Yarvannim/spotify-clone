a2fa5d612120532a5ed7447820e9f68ca68648b0
yardi <yardivn@live.nl>
2025-09-21 20:04:15 +0200

    Setup compose.yaml with MinIO and include test to see if performance has increased

--- stream-service/k6/tests/load/stream-load-test.js
+++ stream-service/k6/tests/load/stream-load-test.js
@@ -1,29 +1,48 @@
-import http from 'k6/http'
-import {check,sleep} from 'k6'
+import http from 'k6/http';
+import { check, sleep } from 'k6';
 
-export let options = {
-    stages: [
-        { duration: '1s', target: 10},
-        { duration: '3s', target: 50},
-        { duration: '3s', target: 100},
-        { duration: '3s', target: 250},
-        { duration: '3s', target: 500},
-        { duration: '3s', target: 1000},
-        { duration: '6s', target: 2000},
-    ],
-    thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-        http_req_duration: ['p(100)<500'], // 100% of requests should be below 500ms
+const minioUrl = 'http://localhost:9001/api/v1/download-shared-object/aHR0cDovLzEyNy4wLjAuMTo5MDAwL211c2ljL3NvbmcubXAzP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9TTdXSlBHRVowUDlaMTczUjFJMTYlMkYyMDI1MDkxOSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNTA5MTlUMTAxMDU5WiZYLUFtei1FeHBpcmVzPTQzMjAwJlgtQW16LVNlY3VyaXR5LVRva2VuPWV5SmhiR2NpT2lKSVV6VXhNaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpoWTJObGMzTkxaWGtpT2lKTk4xZEtVRWRGV2pCUU9Wb3hOek5TTVVreE5pSXNJbVY0Y0NJNk1UYzFPRE14T1RjMU1Dd2ljR0Z5Wlc1MElqb2ljM0J2ZEdsbWVTMWhaRzFwYmlKOS5SaFVyOWNyaXBKWnJDcEMzRWFJYW1SY2JGNkVwZHcwTU9GdC1sUVdfckVMaG0xRXFIOG5HRzJDYTZ5ZVQxVDdpMF9pMVRiYnMxUkxKaGxuWG0yWEw4dyZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmdmVyc2lvbklkPW51bGwmWC1BbXotU2lnbmF0dXJlPWNiYzY5Y2VhZTUxYjViZTU5NThiYWFlYmM5NjQ5MjA0NWI3ZjQ2ZGQ3NmY4NmRlYjgzMTZhNmM3MDU1YjUzZjU';
+
+export const options = {
+    discardResponseBodies: {
+        threshold: '0.1s',
+        body: true,
     },
-}
-export default function (){
-    let response = http.get('http://localhost:8080/stream/song');
+    scenarios: {
+        find_my_bottleneck: {
+            executor: 'ramping-arrival-rate',
+            preAllocatedVUs: 200,
+            maxVUs: 20000,
 
-    check(response,{
-        'is status 200': (r) => r.status === 200,
-        'has correct content type': (r) => r.headers['Content-Type'] === 'audio/mpeg',
-        'response has data': (r) => r.body.length > 0,
+            timeUnit: '1s',
+            startRate: 1,
+
+            stages: [
+                { target: 10, duration: '10s' },
+                { target: 50, duration: '10s' },
+                { target: 100, duration: '10s' },
+                { target: 300, duration: '20s' },
+                { target: 500, duration: '20s' },
+                { target: 750, duration: '20s' },
+                { target: 1000, duration: '30s' },
+            ],
+        },
+    },
+    thresholds: {
+        http_req_failed: [{ threshold: 'rate<0.01', abortOnFail: false }],
+        http_req_duration: [{threshold: 'p(100)<500', abortOnFail: false}],
+    },
+};
+
+export default function () {
+    // A simple GET request to download the object
+    let res = http.get(minioUrl);
+
+    // Track whether this request was successful and fast enough
+    check(res, {
+        'is status 200 or 206': (r) => r.status === 200 || r.status === 206,
     });
 
-    sleep(1);
+    // Optional: Add a very short sleep to be slightly more realistic.
+    // sleep(0.1);
 }
